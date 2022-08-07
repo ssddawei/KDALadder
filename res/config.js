3473675ecf0eed47c9d8285d8885b7b3bd36ede2
weijh <only-wjh2@163.com>
2022-08-07 05:41:05 +0000

    fix config.js

--- res/config.js
+++ res/config.js
@@ -6,5 +6,5 @@ let CONFIG = {
     // accessKeyId: "",
     // accessKeySecret: "",
   },
-  DataUrl: "//kdaladder-dev.oss-cn-guangzhou.aliyuncs.com/"
+  DataUrl: "//kdaladder.oss-cn-guangzhou.aliyuncs.com/"
 }
