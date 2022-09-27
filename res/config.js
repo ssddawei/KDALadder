93899e673de01145f3bcc9c74342469bba517375
weijh <weijh@vgemv.com>
2022-09-28 00:21:27 +0800

    progress

--- res/config.js
+++ res/config.js
@@ -1,5 +1,5 @@
 let CONFIG = {
-  ServerUrl: "http://localhost:8080",
+  ServerUrl: `http://${location.hostname}:8080`,
   AliyunOSSKey: {
     region: "oss-cn-guangzhou",
     bucket: "kdaladder-dev",
